3bbf98534569f34c2baa8fa0adbec76d5b6fcba1
Unais Vayani <unais.vayani@ibex.co>
2024-02-11 02:58:35 +0500

    Initial setup application

--- Backend/WebAPI/WebApi/WeatherForecast.cs
+++ Backend/WebAPI/WebApi/WeatherForecast.cs
@@ -1,12 +1,13 @@
-namespace WebApi.Controllers;
-
-public class WeatherForecast
+namespace webapi
 {
-    public DateTime Date { get; set; }
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
 
-    public int TemperatureC { get; set; }
+        public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+        public string? Summary { get; set; }
+    }
 }
